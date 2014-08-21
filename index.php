035da8cc8b47236f770e60c2a046f1abf430af32
okisanjp <okisan.jp@gmail.com>
2014-08-21 17:05:56 +0900

    min-width設定

--- index.php
+++ index.php
@@ -62,11 +62,11 @@ if(PEAR::isError($xml)){
     $afflink = $data['DetailPageURL'];
 
     $html = '';
-    $html .= '<div style="width:500px;border:1px solid #999;background:#fefefe;margin:16px 0;padding:4px;border-radius:3px;">';
-    $html .= '<div style="width:170px;float:left;">';
+    $html .= '<div style="width:100%;border:1px solid #999;background:#fefefe;margin:16px 0;padding:4px;border-radius:3px;">';
+    $html .= '<div style="min-width:120px;float:left;">';
     $html .= '<a href="' . $afflink . '" target="_blank"><img src="' . $data['MediumImage'] ['URL'] . '" alt="" style="border:1px solid #eee;" /></a>';
     $html .= '</div>';
-    $html .= '<div style="width:330px;float:left;">';
+    $html .= '<div style="min-width:330px;float:left;">';
     $html .= '<a href="' . $afflink . '" target="_blank">' . $data['ItemAttributes']['Title'] . '</a>';
     $html .= $price;
     $html .= $release_date;
